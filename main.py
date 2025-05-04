8aee28ece19f38a6ae84455ec4fb5c5e30e72e37
azimullah <mirza.mnnit@gmail.com>
2025-05-04 11:56:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+a= 34
+b= 52
+c= 0
+
+if a and b and c:
+    print("all the numbers have booleon value as true")  
+else: 
+    print("atleast one number has booleon  value as false")
+
+a= 52
+b= 88
+c= 23
+ 
+if a or > 0 or b > 0:
+    print("either of the number is greater than zero")
+else:
+    print("no number is greater than zero ")
+ 
+
+
+if b >0 > c:
+    print("either of the number is greater than zero")
+else:
+    print("no number is greater than zero ")
