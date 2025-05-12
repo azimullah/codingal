6c88f79c66fc3f9ef90c4939f40189dda6784902
azimullah <mirza.mnnit@gmail.com>
2025-05-12 12:25:14 +0530

    Add files via upload

---
+++ challenge .py
@@ -0,0 +1,15 @@
+x = 8
+y = 2
+w = 5
+z = 0
+v = 4
+z= ( v+w ) *x/y
+print("value of", z)
+
+name=input(" enter the username")
+age=0
+
+if name== input() or "Jhon" and age>= 2:
+    print ("Hi hello")
+else:
+    print ("By goodbye")
